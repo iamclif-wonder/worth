2e3cc7ff0c7273329ad718c2369561e2f77d05f0
Clif Jordan <developer2@findwonder.com>
2011-09-16 13:42:50 -0400

    initial upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Author: 
+
+*/
+$(document).ready(function(){
+
+	$(".btn-slide").click(function(){
+		$("#panel").slideToggle("slow");
+		$(this).toggleClass("active"); return false;
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
