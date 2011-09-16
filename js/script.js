a117fbaab11d392b717977d5f445350e6a639144
Clif Jordan <developer2@findwonder.com>
2011-09-16 17:48:53 -0400

    Add images and started the structural markup.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@
 $(document).ready(function(){
 
 	$(".btn-slide").click(function(){
-		$("#panel").slideToggle("slow");
+		$(".panel").slideToggle("slow");
+		$(".panel2").slideToggle("slow");
 		$(this).toggleClass("active"); return false;
 	});
 
