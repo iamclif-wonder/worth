9b880625637b81c8f11c5731e18f1dc2e69957ff
Clif Jordan <developer2@findwonder.com>
2011-09-19 17:51:46 -0400

    Added new images and refined page structure.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-/* Author: 
-
-*/
 $(document).ready(function(){
 
-	$(".btn-slide").click(function(){
-		$(".panel").slideToggle("slow");
-		$(".panel2").slideToggle("slow");
-		$(this).toggleClass("active"); return false;
+	//Hide (Collapse) the toggle containers on load
+	$(".toggle_container").hide(); 
+
+	//Switch the "Open" and "Close" state per click then slide up/down (depending on open/close state)
+	$(".trigger").click(function(){
+		$(this).toggleClass("active").next().slideToggle("slow");
+		return false; //Prevent the browser jump to the link anchor
 	});
 
 });
