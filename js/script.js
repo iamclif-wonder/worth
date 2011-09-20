ba6cc563043fd7c7fae81038ef83497d63855e96
Clif Jordan <developer2@findwonder.com>
2011-09-20 14:44:59 -0400

    added hover and new rankings with html numbers

--- js/script.js
+++ js/script.js
@@ -11,6 +11,22 @@ $(document).ready(function(){
 
 });
 
+/*
+$(function() {
+      $('toggle_container').hide();
+      $('.power').click(function() { // When an item is clicked
+            var id = $(this).attr('rel'); // Retrieve its content ID
+            var showing = $(id).is(':visible'); // Remember if that content was already visible
+            $('div.hidable').slideUp('slow'); // Hide them all
+            $('.power').removeClass('active'); // Mark all as inactive
+            if (!showing) { // If it wasn't visible
+                  $(id).slideDown('fast'); // Show it
+                  $(this).addClass('active'); // Mark link as active
+            }
+      });
+});
+
+*/
 
 
 
